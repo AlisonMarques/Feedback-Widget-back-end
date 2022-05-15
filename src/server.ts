a27c06aca1c42cafc089911e827121b23c93fd5e
Alison Marques <alison.marques@anbetec.com.br>
2022-05-14 23:38:29 -0300

    desenvolvido a rota para criar feedbacks

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,24 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { prisma } from './prisma';
 
 const app = express();
 
-const port = 3334
+app.use(express.json())
+
+const port = 3333
+
+app.post('/feedbacks', async (req, res) => {
+  const { type, comment, screenshot } = req.body
+
+  const feedback = await prisma.feedback.create({
+    data: {
+      type,
+      comment,
+      screenshot,
+    }
+  })
+  return res.status(201).json({ data: feedback })
+})
 
 app.listen(port, () => {
   console.log(`API rodando na porta ${port}`)
